30342bb888b0f478f03cb2044248111e0d668f5b
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-17 15:28:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,7 @@ def all_holidays_with_bbq(holiday_hash)
     holiday_hash.each do |season, holiday|
       holiday.each do |event, supply|
         if supply.include?("BBQ")
-          list << event
+          bbq << event
         end
       end
     end
